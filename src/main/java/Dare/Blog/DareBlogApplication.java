b4cdc774c478a5461494b800dcb5baab985cd46a
형찬 <gudcks305@gmail.com>
2021-10-30 14:11:43 +0900

    first commit

---
+++ src/main/java/Dare/Blog/DareBlogApplication.java
@@ -0,0 +1,13 @@
+package Dare.Blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DareBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DareBlogApplication.class, args);
+    }
+
+}
